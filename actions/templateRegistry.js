fcfbc9298ee350949a62ffc359e30256b0608b12
Arjun Gupta <arjung@adobe.com>
2024-04-17 16:19:35 +0530

    install template API impl

--- actions/templateRegistry.js
+++ actions/templateRegistry.js
@@ -22,6 +22,7 @@ let openReviewIssues = null;
 
 const { mongoConnection } = require('../db/mongo');
 const { convertMongoIdToString } = require('./utils');
+const { ObjectId } = require('mongodb');
 
 /**
  * Returns a template record from Template Registry by a template name.
@@ -34,6 +35,20 @@ async function findTemplateByName (dbParams, templateName) {
   const collection = await mongoConnection(dbParams, collectionName);
   const results = await collection.find({ name: templateName }).toArray();
   return results?.length ? convertMongoIdToString(results[0]) : null;
+}
+
+/**
+ * Returns a template record from Template Registry by a template id.
+ *
+ * @param {object} dbParams database connection parameters
+ * @param {string} templateId template id
+ * @returns {Promise<object|null>} an existing template record or {}
+ */
+async function findTemplateById (dbParams, templateId) {
+  const collection = await mongoConnection(dbParams, collectionName);
+  const _id = new ObjectId(templateId);
+  const result = await collection.find({ _id }).toArray();
+  return Object.values(result).length ? convertMongoIdToString(result) : null;
 }
 
 /**
@@ -174,6 +189,7 @@ module.exports = {
   fetchUrl,
   getTemplates,
   findTemplateByName,
+  findTemplateById,
   addTemplate,
   removeTemplateByName,
   createReviewIssue,
--- actions/templates/install/index.js
+++ actions/templates/install/index.js
@@ -8,3 +8,229 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
+
+const { Core } = require('@adobe/aio-sdk');
+const { errorResponse, errorMessage, getBearerToken, stringParameters, checkMissingRequestInputs, ERR_RC_SERVER_ERROR, ERR_RC_HTTP_METHOD_NOT_ALLOWED, ERR_RC_INVALID_IMS_ACCESS_TOKEN, ERR_RC_INCORRECT_REQUEST, ERR_RC_INVALID_TEMPLATE_ID } = require('../../utils');
+const { validateAccessToken } = require('../../ims');
+const { findTemplateById } = require('../../templateRegistry');
+const Enforcer = require('openapi-enforcer');
+const consoleLib = require('@adobe/aio-lib-console');
+
+const HTTP_METHOD = 'post';
+const POST_PARAM_NAME = 'templateId';
+
+/**
+ * Serialize the request body
+ * @param {object} params action params
+ * @returns {object} serialized request body
+ */
+const serializeRequestBody = (params) => {
+  // Extracting required properties
+  const { orgId, projectName, description, metadata } = params;
+  // Constructing the serialized object
+  return {
+    orgId,
+    projectName,
+    ...(description && { description }), // Include description if it exists
+    metadata // Include metadata object
+  };
+};
+
+/**
+ * Install a template - create Developer Console project with required credentials and APIs.
+ * @param {object} params request parameters
+ * @returns {object} response
+ */
+
+/**
+ *
+ * @param params
+ */
+async function main (params) {
+  // create a Logger
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' });
+
+  const imsUrl = params.IMS_URL;
+  const imsClientId = params.IMS_CLIENT_ID;
+
+  const dbParams = {
+    MONGODB_URI: params.MONGODB_URI,
+    MONGODB_NAME: params.MONGODB_NAME
+  };
+
+  try {
+    // 'info' is the default level if not set
+    logger.info('Calling "POST install template"');
+
+    // log parameters, only if params.LOG_LEVEL === 'debug'
+    logger.debug(stringParameters(params));
+
+    if (params.__ow_method === undefined || params.__ow_method.toLowerCase() !== HTTP_METHOD) {
+      return errorResponse(405, [errorMessage(ERR_RC_HTTP_METHOD_NOT_ALLOWED, `HTTP "${params.__ow_method}" method is unsupported.`)], logger);
+    }
+
+    // check for missing request input parameters and headers
+    const requiredHeaders = ['Authorization'];
+    let errorMessages = checkMissingRequestInputs(params, [], requiredHeaders);
+    if (errorMessages) {
+      return errorResponse(401, errorMessages, logger);
+    }
+    const requiredParams = [
+      POST_PARAM_NAME
+    ];
+    errorMessages = checkMissingRequestInputs(params, requiredParams);
+    if (errorMessages) {
+      return errorResponse(400, errorMessages, logger);
+    }
+
+    // extract the user Bearer token from the Authorization header
+    const accessToken = getBearerToken(params);
+
+    try {
+      // validate the token, an exception will be thrown for a non-valid token
+      await validateAccessToken(accessToken, imsUrl, imsClientId);
+    } catch (error) {
+      return errorResponse(401, [errorMessage(ERR_RC_INVALID_IMS_ACCESS_TOKEN, error.message)], logger);
+    }
+
+    Enforcer.v3_0.Schema.defineDataTypeFormat('string', 'uuid', null);
+    Enforcer.v3_0.Schema.defineDataTypeFormat('string', 'uri', null);
+
+    // WPAR002 - skip a warning about the "allowEmptyValue" property
+    // see https://swagger.io/docs/specification/describing-parameters/ Empty-Valued and Nullable Parameters
+    // EDEV001 - skip a warning about the basepath property, needed by IO Runtime for deploying apis
+    const openapi = await Enforcer('./template-registry-api.json', { componentOptions: { exceptionSkipCodes: ['WPAR002', 'EDEV001'] } });
+
+    const body = serializeRequestBody(params);
+
+    logger.debug(`Request body: ${JSON.stringify(body)}`);
+
+    const [req, reqError] = openapi.request({
+      method: HTTP_METHOD,
+      path: '/templates/{templateId}/install',
+      body
+    });
+    if (reqError) {
+      return errorResponse(400, [errorMessage(ERR_RC_INCORRECT_REQUEST, reqError.toString().split('\n').map(line => line.trim()).join(' => '))], logger);
+    }
+
+    const template = await findTemplateById(dbParams, params.templateId);
+    if (!template) {
+      return errorResponse(404, [errorMessage(ERR_RC_INVALID_TEMPLATE_ID, `Template with id ${params.templateId} not found.`)], logger);
+    }
+    logger.debug(`Template found: ${JSON.stringify(template)}`);
+
+    // extract credentials and APIs from the template
+    const { credentials, apis } = template;
+
+    logger.debug(`Credentials found: ${JSON.stringify(credentials)}`);
+    logger.debug(`APIs found: ${JSON.stringify(apis)}`);
+
+    // credential types could be: 'oauth_server_to_server', 'apikey', 'oauthnativeapp', 'oauthwebapp', 'oauthsinglepageapp'
+    // credential flow types could be: 'adobeid', 'entp'
+    // adobeid is used for OAuth creds and entp is used for OAuthS2S and JWT creds
+
+    const mapAdobeIdCredentialTypeToPlatformType = {
+      apikey: 'apiKey',
+      oauthnativeapp: 'NativeApp',
+      oauthwebapp: 'WebApp',
+      oauthsinglepageapp: 'SinglePageApp'
+    };
+
+    const consoleClient = await consoleLib.init(accessToken, params.IMS_CLIENT_ID, 'stage');
+
+    // Console APIs only support creating one type of credential at a time
+    // Also, for current templates, we only support one credential type per template
+    // So, assuming only one credential present in credentials array
+
+    const credentialType = credentials[0].type;
+    const credentialFlowType = credentials[0].flowType;
+    let createIntegrationResponse = {};
+
+    if (credentialFlowType === 'adobeid') {
+      // form integration request body
+      const createAdobeIdIntegrationReqBody = {
+        name: String(body.projectName),
+        description: String(body.description ? body.description : `Created from template ${template.name}`),
+        platform: String(mapAdobeIdCredentialTypeToPlatformType[credentialType]),
+        ...(body.metadata?.urlScheme && { urlScheme: body.metadata.urlScheme }), // Include urlScheme if it exists
+        ...(body.metadata?.redirectUriList && { redirectUriList: body.metadata.redirectUriList }), // Include redirectUriList if it exists
+        ...(body.metadata?.defaultRedirectUri && { defaultRedirectUri: body.metadata.defaultRedirectUri }), // Include defaultRedirectUri if it exists
+        ...(body.metadata?.domain && { domain: body.metadata.domain }), // Include domain if it exists
+        templateId: params.templateId,
+        services: []
+      };
+
+      logger.debug(`Create AdobeId Integration Request Body: ${JSON.stringify(createAdobeIdIntegrationReqBody)}`);
+
+      // iterate over APIs and add APIs to request body services array
+      for (const api of apis) {
+        if (api.flowType !== 'adobeid' || api.credentialType !== credentialType) {
+          continue;
+        }
+        const service = {
+          sdkCode: api.code,
+          atlasPlanCode: '',
+          licenseConfigs: [],
+          roles: []
+        };
+        createAdobeIdIntegrationReqBody.services.push(service);
+      }
+
+      logger.debug(`Create AdobeId Integration Request Body after adding services: ${JSON.stringify(createAdobeIdIntegrationReqBody)}`);
+
+      // create AdobeID integration
+      createIntegrationResponse = await consoleClient.createAdobeIdIntegration(body.orgId, createAdobeIdIntegrationReqBody);
+      logger.debug(`AdobeID Integration created: ${JSON.stringify(createIntegrationResponse)}`);
+    } else if (credentialFlowType === 'entp') {
+      // form integration request body
+      const createOAuthS2SIntegrationReqBody = {
+        name: String(body.projectName),
+        description: String(body.description ? body.description : `Created from template ${template.name}`),
+        templateId: params.templateId,
+        services: []
+      };
+
+      logger.debug(`Create OAuthS2S Integration Request Body: ${JSON.stringify(createOAuthS2SIntegrationReqBody)}`);
+
+      for (const api of apis) {
+        if (api.flowType !== 'entp' || api.credentialType !== credentialType) {
+          continue;
+        }
+        const service = {
+          sdkCode: api.code,
+          atlasPlanCode: '',
+          licenseConfigs: [],
+          roles: []
+        };
+        createOAuthS2SIntegrationReqBody.services.push(service);
+      }
+
+      // create Entp integration
+      createIntegrationResponse = await consoleClient.createOauthS2SCredential(body.orgId, createOAuthS2SIntegrationReqBody);
+      logger.debug(`OAuth S2S Integration created: ${JSON.stringify(createIntegrationResponse)}`);
+    }
+    const { projectId, workspaceId } = createIntegrationResponse;
+    logger.debug(`ProjectId: ${projectId}, WorkspaceId: ${workspaceId}`);
+
+    // call download workspace config API to get the config
+    const response = await consoleClient.downloadWorkspaceJson(body.orgId, projectId, workspaceId);
+    logger.debug(`Workspace config: ${JSON.stringify(response)}`);
+
+    // validate the response data to be sure it complies with OpenApi Schema
+    const [res, resError] = req.response(201, response);
+    if (resError) {
+      throw new Error(resError.toString());
+    }
+
+    logger.info('"POST Install templates" executed successfully');
+    return {
+      statusCode: 201,
+      body: res.body
+    };
+  } catch (error) {
+    logger.error(error);
+    return errorResponse(500, [errorMessage(ERR_RC_SERVER_ERROR, error.message)], logger);
+  }
+}
+exports.main = main;
